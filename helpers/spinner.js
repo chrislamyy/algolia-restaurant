8d153d1425a5f1cc85b2a1a82096cefdab4c5724
chrislamyy <chrislamyy@gmail.com>
2024-05-07 18:08:33 +0100

    Updates

--- helpers/spinner.js
+++
@@ -1,8 +0,0 @@
-// spinner helper functions
-export function showSpinner() {
-  document.getElementById("spinner-container").classList.remove("d-none");
-}
-
-export function hideSpinner() {
-  document.getElementById("spinner-container").classList.add("d-none");
-}
